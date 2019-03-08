e7eace51bcabf19248efa4d2f8c18cdbbe1ae922
Stefan Penner <stefan.penner@gmail.com>
2019-03-08 12:01:59 -0800

    Enable embroider and demonstrate failure

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,5 +21,9 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  const Webpack = require('@embroider/webpack').Webpack;
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    compatAdapters: new Map([['ember-decorators', null]])
+  });
+  // return app.toTree();
 };
